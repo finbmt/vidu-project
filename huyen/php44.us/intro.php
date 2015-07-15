2e5b6ddb7fe6f3d877a3e515903aee41c488054b
huyenpham29611 <huyenpham29611@5e27f482-3e6c-c57d-b3f5-cb6f90b35367>
2015-07-15 11:49:30 +0000

    bai tap

---
+++ huyen/php44.us/intro.php
@@ -0,0 +1,42 @@
+
+<?php
+/*tính tổng*/
+echo "cau 1 ";
+$a=array(1,2,3,4,5,6,7,8,9,10);
+echo array_sum($a);
+?>
+
+<?php
+echo "<br/>";
+echo "cau 2  ";
+$a=array(1,2,3,4,5,6,7,8,9,10);
+echo min($a);
+?>  
+
+
+<?php
+echo "<br/>";
+echo "cau 3  ";
+$a=array(1,2,3,4,5,6,7,8,9,10);
+echo max($a);
+?> 
+
+
+<?php
+echo "<br/>";
+echo "cau 4  ";
+$a=array(1,2,3,4,5,6,7,8,9,10);
+echo array_shift($a);
+
+?>
+
+<?php
+echo "<br/>";
+echo "cau 5  ";
+$a=array(1,2,3,4,5,6,7,8,9,10);
+  array_pop($a);
+var_dump($a);
+?>
+
+
+	
